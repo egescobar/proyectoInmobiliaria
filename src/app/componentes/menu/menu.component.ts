7c39ca2ea117950b78856f1d795728f5e4b80bea
Exequiel Ledesma <Exequiel Ledesma>
2017-06-04 16:15:22 -0300

    Menu Component

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -1,22 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { rol } from './rol';
+
+
+import { AutService } from '../../service/auth/aut.service';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+ rol: rol;
+  Aut:AutService;
+  usuario:string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+this.Aut = new AutService(router);
+
+   }
 
   ngOnInit() {
+    this.rol = new rol();
+    this.rol =this.Aut.getRol();
+    console.log(this.Aut.getRol());
+    this.usuario = this.Aut.getToken().nombre;
   }
   salir()
   {
     localStorage.setItem('token', null);
-    window.alert('Chauuuuuu!!!');
-    this.router.navigate(['/login']);
+    localStorage.setItem('rol', null);
+  
+    this.router.navigate(['/']);
+  }
+
+   IrAltaProducto()
+  {
+    this.router.navigate(['/alta-producto']);
   }
 
 }
---
+++ src/app/componentes/menu/rol.ts
@@ -0,0 +1,9 @@
+export class rol {
+  id_rol: number;
+  descripcion: string;
+  crear_usuario: boolean;
+  crear_sucursal: boolean;
+  modificar_producto: boolean;
+  crear_producto: boolean;
+  ver_ofertas: boolean;
+}
