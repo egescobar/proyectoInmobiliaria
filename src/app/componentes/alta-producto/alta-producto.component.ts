5eb8f28b038f7f08cdf8994a79e901eef16dae58
Exequiel Ledesma <Exequiel Ledesma>
2017-06-04 14:28:41 -0300

    Alta Productos

---
+++ src/app/componentes/alta-producto/alta-producto.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-alta-producto',
+  templateUrl: './alta-producto.component.html',
+  styleUrls: ['./alta-producto.component.css']
+})
+export class AltaProductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/componentes/alta-producto/altaProducto.ts
@@ -0,0 +1,9 @@
+export class producto {
+  id_producto: number;
+  tipo_producto: string;
+  direccion: string;
+  descripcion: string;
+  moneda: string;
+  precio: number;
+  sucursal:string;
+}
