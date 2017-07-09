8139026b627667f5b446ed831d0b6af184ccdb60
egescobar <graciela_escobar_18@hotmail.com>
2017-07-09 16:46:41 -0300

    validaciones, marcar dos rutas en mapa

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { Component, NgModule, NgZone, OnInit, ViewChild, ElementRef, Directive, Input  } from '@angular/core';
+import { FormGroup, AbstractControl, FormBuilder, Validators,FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 
@@ -8,11 +8,21 @@ import { WsService } from '../../service/ws/ws.service';
 import { AutService } from '../../service/auth/aut.service';
 import { sucursalesMap } from './sucursalesMap';
 
+import { BrowserModule } from "@angular/platform-browser";
+import { AgmCoreModule, MapsAPILoader, GoogleMapsAPIWrapper } from '@agm/core';
+import { DirectionsMapDirective } from './googlr-map.directive';
+
+import{} from '@types/googlemaps';
+
+
+declare var google:any;
+declare var jQuery:any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+   providers : [ GoogleMapsAPIWrapper ]
 })
 export class LoginComponent implements OnInit {
   title = 'Inmobiliaria El Campito S.A.';
@@ -29,9 +39,35 @@ export class LoginComponent implements OnInit {
   lat: number = -34.6083;
   lng: number = -58.3712;
 
+ public latitude: number;
+    public longitude: number;
+    public destinationInput: FormControl;
+    public destinationOutput: FormControl;
+    public zoom: number;
+    public iconurl: string;
+    public mapCustomStyles : any;
+    public estimatedTime: any;
+    public estimatedDistance: any;
 
+    @ViewChild("pickupInput")
+    public pickupInputElementRef: ElementRef;
 
-  constructor(private router: Router, private ws: WsService) {
+     @ViewChild("pickupOutput")
+    public pickupOutputElementRef: ElementRef;
+
+     @ViewChild("scrollMe")
+    private scrollContainer: ElementRef;
+
+    @ViewChild(DirectionsMapDirective) vc: DirectionsMapDirective;
+
+    public origin :any ; // its a example aleatory position
+    public destination : any; // its a example aleatory position
+
+  constructor(private router: Router, private ws: WsService,
+  private mapsAPILoader: MapsAPILoader,
+      private ngZone: NgZone,
+      private gmapsApi: GoogleMapsAPIWrapper,
+      private _elementRef : ElementRef) {
 
     this.Aut = new AutService(router);
 
@@ -44,9 +80,100 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+//set google maps defaults
+      this.zoom = 4;
+      this.latitude = 39.8282;
+      this.longitude = -98.5795;
+      //this.iconurl = '../image/map-icon.png';
+      this.iconurl = '../image/map-icon.png';
 
+     // this.mapCustomStyles = this.getMapCusotmStyles();
+      //create search FormControl
+      this.destinationInput = new FormControl();
+      this.destinationOutput = new FormControl();
+      //set current position
+      this.setCurrentPosition();
+      
+      //load Places Autocomplete
+      this.mapsAPILoader.load().then(() => {
+          let autocompleteInput = new google.maps.places.Autocomplete(this.pickupInputElementRef.nativeElement, {
+            types: ["address"]
+          });
+
+          let autocompleteOutput = new google.maps.places.Autocomplete(this.pickupOutputElementRef.nativeElement, {
+            types: ["address"]
+          });
+        
+                 this.setupPlaceChangedListener(autocompleteInput, 'ORG');
+                this.setupPlaceChangedListener(autocompleteOutput, 'DES');
+      });
 
   }
+ private setupPlaceChangedListener(autocomplete: any, mode: any ) {
+      autocomplete.addListener("place_changed", () => {
+            this.ngZone.run(() => {
+              //get the place result
+              let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+              //verify result
+              if (place.geometry === undefined) {
+                return;
+              }
+              if (mode === 'ORG') {
+                  this.vc.origin = { longitude: place.geometry.location.lng(), latitude: place.geometry.location.lat() }; 
+                  this.vc.originPlaceId = place.place_id;
+              } else {
+                  this.vc.destination = { longitude: place.geometry.location.lng(), latitude: place.geometry.location.lat() }; // its a example aleatory position
+                  this.vc.destinationPlaceId = place.place_id;
+              }
+  
+              if(this.vc.directionsDisplay === undefined){ this.mapsAPILoader.load().then(() => { 
+                    this.vc.directionsDisplay = new google.maps.DirectionsRenderer;
+                  }); 
+            }
+          
+              //Update the directions
+              this.vc.updateDirections();
+              this.zoom = 12;
+            });
+
+         });
+
+    }
+
+    getDistanceAndDuration(){
+      this.estimatedTime = this.vc.estimatedTime;
+      this.estimatedDistance = this.vc.estimatedDistance;
+    }
+
+    scrollToBottom(): void {
+      jQuery('html, body').animate({ scrollTop: jQuery(document).height() }, 3000);
+    }
+    private setPickUpLocation( place:any ) {
+      //verify result
+            if (place.geometry === undefined || place.geometry === null) {
+              return;
+            }
+            //set latitude, longitude and zoom
+            this.latitude = place.geometry.location.lat();
+            this.longitude = place.geometry.location.lng();
+            this.zoom = 12;
+    }
+
+    private setCurrentPosition() {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+          this.zoom = 12;
+        });
+      }
+    }
+
+    private getMapCusotmStyles() {
+      // Write your Google Map Custom Style Code Here.
+    }
+
+
 
   enviar() {
     this.ws.get(this.login)
